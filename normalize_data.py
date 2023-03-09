f53c65ed737120760ef9ac7d06ddad71db3aa5b3
Mateusz Gula <mateuszgua@wp.eu>
2023-03-09 18:13:04 +0100

    Fix: clear data, add date in correct format, chnage referee for only last name, replace team name for team id in games table

--- normalize_data.py
+++ normalize_data.py
@@ -6,31 +6,54 @@ import numpy as np
 os.chdir("./data")
 
 all_data = pd.read_csv('append_files.csv')
-# print(all_data)
+print(all_data)
 
 
 def get_club_names():
-    df = all_data['HomeTeam'].unique()
-    df_club_names = pd.DataFrame(df, columns=['Name'])
+    unique_values = all_data['HomeTeam'].unique()
+    cleared_nan = [x for x in unique_values if str(x) != 'nan']
+    club_names = pd.DataFrame(cleared_nan, columns=['Name'])
 
     filename = "club_names.csv"
     files_present = glob.glob(filename)
 
     if not files_present:
-        df_club_names.to_csv(filename, index=True,
-                             index_label='Id', encoding='utf-8-sig')
+        club_names.to_csv(filename, index=True,
+                          index_label='Id', encoding='utf-8-sig')
         return 'File save successfully!'
     else:
         return 'File already exists!'
 
 
 response = get_club_names()
-# print(response)
+print(response)
 
 
 def get_referee_names():
-    df = all_data['Referee'].unique()
-    df_referee_names = pd.DataFrame(df, columns=['Name'])
+    unique_values = all_data['Referee'].unique()
+    cleared_nan = [x for x in unique_values if str(x) != 'nan']
+
+    splited_names = []
+    for i in cleared_nan:
+        splited = i.split()
+        splited_names.append(splited)
+
+    lastnames = []
+    for lastname in splited_names:
+        x = lastname[0].find(',')
+        if x != -1:
+            lastnames.append(lastname[0].replace(',', ''))
+        elif (len(lastname[0]) <= 2) and (len(lastname[1]) != 1 and len(lastname[1]) != 2):
+            lastnames.append(lastname[1])
+        elif (len(lastname[0]) <= 2) and (len(lastname[1]) <= 2):
+            lastnames.append(lastname[2])
+        elif (len(lastname[0]) > 1) and (len(lastname[1]) <= 2):
+            lastnames.append(lastname[0])
+        elif len(lastname[0]) > 1 and len(lastname[1]) > 1:
+            lastnames.append(lastname[1])
+
+    lastnames = [*set(lastnames)]
+    df_referee_names = pd.DataFrame(lastnames, columns=['Name'])
 
     filename = "referee_names.csv"
     files_present = glob.glob(filename)
@@ -49,14 +72,14 @@ print(response)
 
 def get_bets():
     bet_table = all_data[all_data.columns[23:68]]
-    # df_referee_names = pd.DataFrame(bet_table, columns=['Name'])
+    cleared_nan = bet_table.fillna('Null')
 
     filename = "bet_table.csv"
     files_present = glob.glob(filename)
 
     if not files_present:
-        bet_table.to_csv(filename, index=True,
-                         index_label='Id', encoding='utf-8-sig')
+        cleared_nan.to_csv(filename, index=True,
+                           index_label='Id', encoding='utf-8-sig')
         return 'File save successfully!'
     else:
         return 'File already exists!'
@@ -67,15 +90,28 @@ print(response)
 
 
 def get_games():
-    df = all_data[all_data.columns[1:23]]
-    df2 = df.drop(['Referee'], axis=1)
+    games_table = all_data[all_data.columns[1:23]]
+    games_drop_referee = games_table.drop(['Referee'], axis=1)
+    cleared_nan = games_drop_referee.fillna('Null')
+
+    club_names = pd.read_csv('club_names.csv')
+
+    cleared_nan['HomeTeam'] = cleared_nan['HomeTeam'].map(
+        club_names.set_index('Name')['Id'])
+    cleared_nan['AwayTeam'] = cleared_nan['AwayTeam'].map(
+        club_names.set_index('Name')['Id'])
+    cleared_nan['Date'] = cleared_nan['Date'].replace(['Null'], '01/01/51')
+    cleared_nan['Date'] = pd.to_datetime(
+        cleared_nan['Date'], errors='coerce', format='%d/%m/%y').dt.strftime('%Y-%m-%d')
+    cleared_nan = cleared_nan.fillna('Null')
+    print(cleared_nan)
 
     filename = "games_table.csv"
     files_present = glob.glob(filename)
 
     if not files_present:
-        df2.to_csv(filename, index=True,
-                   index_label='Id', encoding='utf-8-sig')
+        cleared_nan.to_csv(filename, index=True,
+                           index_label='Id', encoding='utf-8-sig')
         return 'File save successfully!'
     else:
         return 'File already exists!'
