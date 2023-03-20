275d2daf711d287963c10d27b31ba6d4d855659b
Mateusz Gula <mateuszgua@wp.eu>
2023-03-20 16:32:18 +0100

    Feat: add data in documentation view; Fix: change image size in flask view, change source path in views and flask

--- views.py
+++ views.py
@@ -48,45 +48,47 @@ def database():
 @app.route('/flask')
 def flask():
     try:
-        sql_file_task1 = "./sql_files/task_1.sql"
+        sql_file_task1 = "../sql_files/task_1.sql"
         df_task1 = get_dataframe_from_sql(sql_file_task1)
         df_task1.columns = ['Club', 'Wins']
-        sql_file_task2 = "./sql_files/task_2.sql"
+        sql_file_task2 = "../sql_files/task_2.sql"
         df_task2 = get_dataframe_from_sql(sql_file_task2)
         df_task2.columns = ['Club', 'TotalPoints']
-        sql_file_task3 = "./sql_files/task_3.sql"
+        sql_file_task3 = "../sql_files/task_3.sql"
         df_task3 = get_dataframe_from_sql(sql_file_task3)
         df_task3.columns = ['Club', 'TotalGames']
-        sql_file_task4 = "./sql_files/task_4.sql"
+        sql_file_task4 = "../sql_files/task_4.sql"
         df_task4 = get_dataframe_from_sql(sql_file_task4)
         df_task4.columns = ['Season', 'AvgGoals']
-        sql_file_task5 = "./sql_files/task_5.sql"
+        sql_file_task5 = "../sql_files/task_5.sql"
         df_task5 = get_dataframe_from_sql(sql_file_task5)
         df_task5.columns = ['Season', 'NumberDraws']
-        sql_file_task6 = "./sql_files/task_6.sql"
+        sql_file_task6 = "../sql_files/task_6.sql"
         df_task6 = get_dataframe_from_sql(sql_file_task6)
         df_task6.columns = ['Season', 'SumFirstHalfGoals',
                             'SumSecoundHalfGoals', 'AvgFirstHalfGoals', 'AvgSecoundHalfGoals']
-        sql_file_task7 = "./sql_files/task_7.sql"
+        sql_file_task7 = "../sql_files/task_7.sql"
         df_task7 = get_dataframe_from_sql(sql_file_task7)
         df_task7.columns = ['Season', 'AvgShots',
                             'AvgCorners', 'AvgFouls', 'AvgYellowCards', 'AvgRedCards']
-        sql_file_task8 = "./sql_files/task_8.sql"
+        sql_file_task8 = "../sql_files/task_8.sql"
         df_task8 = get_dataframe_from_sql(sql_file_task8)
         df_task8.columns = ['Season', 'Club',
                             'Points', 'Goals', 'Fouls', 'Corners', 'YellowCards', 'RedCards']
-        sql_file_task9 = "./sql_files/task_9.sql"
+        sql_file_task9 = "../sql_files/task_9.sql"
         df_task9 = get_dataframe_from_sql(sql_file_task9)
         df_task9.columns = ['Name', 'NumberMatches']
-        sql_file_task10 = "./sql_files/task_10.sql"
+        sql_file_task10 = "../sql_files/task_10.sql"
         df_task10 = get_dataframe_from_sql(sql_file_task10)
         df_task10.columns = ['Name', 'YellowCards']
-        sql_file_task11 = "./sql_files/task_11.sql"
+        sql_file_task11 = "../sql_files/task_11.sql"
         df_task11 = get_dataframe_from_sql(sql_file_task11)
         df_task11.columns = ['Season', 'Name', 'RedCards']
-        sql_file_task12 = "./sql_files/task_12.sql"
+        sql_file_task12 = "../sql_files/task_12.sql"
         df_task12 = get_dataframe_from_sql(sql_file_task12)
         df_task12.columns = ['NumberMatches', 'IsCorrectType']
+    except Error as e:
+        print(e)
     finally:
         return render_template('flask.html',
                                column_names1=df_task1.columns.values,
@@ -135,7 +137,7 @@ def visualization():
         font2 = {'family': 'serif', 'color': 'black', 'size': 15}
 
         list_numbers = list(range(1, 21))
-        sql_file_task_vis_1_3 = "./sql_files/task_vis_1_3.sql"
+        sql_file_task_vis_1_3 = "../sql_files/task_vis_1_3.sql"
         df_task_vis_1_3 = get_dataframe_from_sql(sql_file_task_vis_1_3)
 
         list_seasons_short = []
@@ -145,10 +147,10 @@ def visualization():
                         for s in list_seasons_short]
         dict_seasons = {list_seasons_short[i]: list_seasons[i]
                         for i in range(len(list_seasons_short))}
-        sql_file_task_vis_1 = "./sql_files/task_vis_1.sql"
+        sql_file_task_vis_1 = "../sql_files/task_vis_1.sql"
         create_sql_procedure(sql_file_task_vis_1)
 
-        sql_file_task_vis_2 = "./sql_files/task_vis_2.sql"
+        sql_file_task_vis_2 = "../sql_files/task_vis_2.sql"
         df_task_vis_2 = get_dataframe_from_sql(sql_file_task_vis_2)
         df_task_vis_2.columns = ['Season', 'Club', 'Points']
 
@@ -171,10 +173,10 @@ def visualization():
         plt.ylabel('Points', fontdict=font2)
         plt.legend(bbox_to_anchor=(1, 1), loc="upper left",
                    frameon=False, fontsize=12)
-        plt.savefig('./static/charts/chart_task_2.png')
+        plt.savefig('../static/charts/chart_task_2.png')
         plt.close()
 
-        sql_file_task_vis_3 = "./sql_files/task_vis_3.sql"
+        sql_file_task_vis_3 = "../sql_files/task_vis_3.sql"
         df_task_vis_3 = get_dataframe_from_sql(sql_file_task_vis_3)
         df_task_vis_3.columns = ['Season', 'Goals']
 
@@ -199,11 +201,10 @@ def visualization():
                          arrowprops=dict(arrowstyle="->", color='black'),
                          fontsize=12)
 
-        plt.savefig('./static/charts/chart_task_3.png')
+        plt.savefig('../static/charts/chart_task_3.png')
         plt.close()
     except Error as e:
         print(e)
-
     finally:
         return render_template('visualization.html', seasons=dict_seasons, positions=list_numbers)
 
@@ -249,12 +250,10 @@ def chart():
                          fontsize=15)
 
         plt.legend(loc='lower center', frameon=False, fontsize=15)
-        plt.savefig('./static/charts/chart_task_1.png')
+        plt.savefig('../static/charts/chart_task_1.png')
         plt.close()
-
     except Error as e:
         print(e)
-
     finally:
         return redirect(url_for('visualization'))
 
@@ -268,6 +267,8 @@ def get_dataframe_from_sql(sql_file_path):
         cursor.execute(sql_as_string)
         result = cursor.fetchall()
         result_df = pd.DataFrame(result)
+    except Error as e:
+        print(e)
     finally:
         sql_file.close()
         return result_df
